981f9ac04b4f4ef42d39847e4c35c3155c36a905
star2wars3 <amiligram@gmail.com>
2016-07-14 11:05:32 -0400

    Add files via upload

---
+++ pages/about/client/about.js
@@ -0,0 +1,13 @@
+const developers = [
+{name:"Matthew Bonnecaze",email:"taddroot@gmail.com"},
+{name:"Aaron Milgram",email:"amiligram@gmail.com"},
+{name:"Max Nobel",email:"empleonme@gmail.com"},
+{name:"Benjamin Siege",email:"ben.siege@gmail.com"},
+{name:"Ash Taber",email:"jtaber27@gmail.com"},
+];
+
+
+Template.about.helpers(
+{developers}
+
+	)
