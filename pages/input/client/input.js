0bc21ec7544384c733137d502cb5f8ea64940f24
Benjamin Siege <ben.siege@gmail.com>
2016-07-15 16:06:27 -0400

    preliminary code on multlists

--- pages/input/client/input.js
+++ pages/input/client/input.js
@@ -21,20 +21,24 @@ Template.input.events(
   
 {
 
+	
+
 
 		"click .js-show-btn": function(event)
 		{//   event.preventdefault();
-			 console.log(Meteor.userId());
+			console.log(Meteor.userId());
 			console.log("clicked the button");
 			console.log("clicked button");
 			const genre = $(".genre").val();
 			const show = $(".show").val();
 			const episode = $(".episode").val();
 			const link = $(".link").val();
+			console.log("id button pushed");
+			const listID =$ (".listID").val();
 			
-			const item = {createdAt:new Date(),mediaForm:genre, name:show, priority:episode, mediaLink:link, userId:Meteor.userId()};
-			console.dir(item);
-			PlayList.insert(item);
+			const item = { createdAt:new Date(),mediaForm:genre, name:show, priority:episode, mediaLink:link, userId:Meteor.userId()};
+			console.dir(item,listID);
+			PlayList.insert(item,listID);
 			Router.go("playlist");
 		},
 
