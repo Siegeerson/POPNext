3bb6972b9a881967f5357b57f06bfd6b205a959a
Benjamin Siege <ben.siege@gmail.com>
2016-07-14 21:48:40 -0400

    changing sorting

--- pages/playlist/client/playlist.js
+++ pages/playlist/client/playlist.js
@@ -2,7 +2,7 @@ Template.playlist.helpers(
 	{
 		userShow:function()
 		{
-			return PlayList.find({},{sort:{priority:-1}}
+			return PlayList.find({},{sort:{priority:1}}
                       
                       )
 		}
